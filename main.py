17ba0bafcaeb50b770ee4902dbd23a5cf7d1a28b
Peter <user@users-Air-2.fritz.box>
2024-01-16 22:41:06 +0000

    New update

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ import utils
 
 # Set the webpage title
 st.markdown("<h1 style='text-align: LEFT; color: #353839; font-weight: LIGHT; font-size: 37px;'>Chocolate Image Classification</h1>", unsafe_allow_html=True)
+
+# Brief introduction of the model
 st.markdown("<h1 style='text-align: LEFT; font-size: 14px;'>This is a simple neural network model that helps classify images of white and dark <br>chocolate. While this model is rudimentary, it serves as a valuable introduction to the <br>concepts of image classification and neural networks. </h1>", unsafe_allow_html=True)
 
 
 # Set overall background color and center image
-background_color = "#381A14"  # Set your desired background color
-center_image_path = "/Users/user/Desktop/Test Folder/testimage7.jpeg"  # Replace with the path to your center image
+background_color = "#381A14"  # Set the desired background color
+center_image_path = "testimage7.jpeg"  # set the background image
 utils.set_app_style(background_color, center_image_path)
 
 # Upload files
@@ -21,8 +23,6 @@ file = st.file_uploader('Upload chocolate images', type=['jpeg', 'jpg', 'png', '
 
 # Load the model
 model = load_model('model.h5')
-
-
 
 # Display image and predictions
 if file is not None:
