3e6492488b15319c4bad6bd17334fcce7e157b6e
Peter <user@users-Air-2.fritz.box>
2024-01-16 22:26:10 +0000

    Add files

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import base64
+import utils
+
+# Set the webpage title
+st.markdown("<h1 style='text-align: LEFT; color: #353839; font-weight: LIGHT; font-size: 37px;'>Chocolate Image Classification</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: LEFT; font-size: 14px;'>This is a simple neural network model that helps classify images of white and dark <br>chocolate. While this model is rudimentary, it serves as a valuable introduction to the <br>concepts of image classification and neural networks. </h1>", unsafe_allow_html=True)
+
+
+# Set overall background color and center image
+background_color = "#381A14"  # Set your desired background color
+center_image_path = "/Users/user/Desktop/Test Folder/testimage7.jpeg"  # Replace with the path to your center image
+utils.set_app_style(background_color, center_image_path)
+
+# Upload files
+file = st.file_uploader('Upload chocolate images', type=['jpeg', 'jpg', 'png', 'webp'])
+
+# Load the model
+model = load_model('model.h5')
+
+
+
+# Display image and predictions
+if file is not None:
+    image = Image.open(file)
+    new_image = image.resize((700, 400))
+    st.image(new_image, use_column_width=True)
+
+    predictions = utils.read_img2(image, model)
+    st.write("## {}".format(predictions))
+
---
+++ utils.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import base64
+import streamlit as st 
+import numpy as np
+
+# Changing the webapp background
+def set_app_style(background_color, center_image_path):
+    # Read the image for centering
+    with open(center_image_path, 'rb') as f:
+        image_data = f.read()
+        image_base64 = base64.b64encode(image_data).decode()
+
+    # Apply custom CSS styles
+    st.markdown(
+        f"""
+        <style>
+        body {{
+            background-color: {background_color};
+        }}
+        .stApp {{
+            background: url(data:image/png;base64,{image_base64}) center center no-repeat;
+            background-size: contain;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+# Making Predictions
+def read_img2(image, model):
+    resize = tf.image.resize(image, (256, 256))
+    yhat = model.predict(np.expand_dims(resize / 255, 0))
+
+    if yhat > 0.5:
+        return 'Predicted class is White Chocolate'
+    else:
+        return 'Predicted class is Dark Chocolate'
